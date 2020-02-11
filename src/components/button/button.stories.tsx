73234518d277f0e44a3792caa7d1a3677db9718e
jescowuester <jesco@storyofams.com>
2020-02-11 11:29:34 +0700

    initial commit

---
+++ src/components/button/button.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Button from './button';
+import './button.css';
+
+export default {
+  title: 'Button',
+};
+
+export const button = () => <Button />;
---
+++ src/components/button/button.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+export interface IButton {}
+export const Button: React.FC<IButton> = ({}) => {
+  return <button data-cml___button>aaaa</button>;
+};
+
+export default Button;
